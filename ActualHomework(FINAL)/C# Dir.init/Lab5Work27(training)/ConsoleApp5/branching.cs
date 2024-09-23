f977cf33acf2ae2fc6d70deb520833b87c4e2f34
George <77775699+TheSkyLand@users.noreply.github.com>
2024-09-23 23:12:23 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/C# Dir.init/Lab5Work27(training)/ConsoleApp5/branching.cs
@@ -0,0 +1,47 @@
+﻿namespace LaboratoryWork5Var27
+{
+    class branching
+    {
+        public double c, y, x, z, l;
+
+        public double systemSolution(double c, double y, double x, double z) //public - значит можно обращаться из любой точки
+        {
+            double l = 15;
+
+            if (y * z > 0 && x > 0)
+            {
+                double max = Math.Sqrt(x);
+
+                if (max < x * y)
+                {
+                    max = c * y;
+                }
+
+                double min = y;
+
+                if (min > max)
+                {
+                    min = max;
+                }
+                if (min > z)
+                {
+                    min = z;
+                }
+
+                l = min;
+            }
+            else if (y * z < 0 && x > 0)
+            {
+                double min = Math.Tan(x);
+
+                if (min > Math.Pow(z, 3))
+                {
+                    min = Math.Pow(z, 3);
+                }
+
+                l = min;
+            }
+            return l;
+        }
+    }
+}
---
+++ ActualHomework(FINAL)/C# Dir.init/Lab5Work27(training)/ConsoleApp5/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork5Var27
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            branching Obj = new branching();
+
+            Console.WriteLine("введите с: ");
+            Obj.c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите y: ");
+            Obj.y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите x: ");
+            Obj.x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите z: ");
+            Obj.z = Convert.ToDouble(Console.ReadLine());
+
+
+            Obj.l = Obj.systemSolution(Obj.c, Obj.y, Obj.x, Obj.z);
+
+            Console.Clear();
+
+            Console.WriteLine("При c = " + Obj.c + " при y = " + Obj.y + " при x = " + Obj.x + "при z = " + Obj.z);
+            Console.WriteLine("Система принимает результат: " + Obj.l);
+
+            Console.ReadLine();
+        }
+    }
+}
