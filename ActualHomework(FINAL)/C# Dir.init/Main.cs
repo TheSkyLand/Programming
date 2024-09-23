3bdf81283f594c1724dca7ee5da4cf2d2d2e0d93
George <77775699+TheSkyLand@users.noreply.github.com>
2024-09-24 00:13:50 +0300

    Lab?Work1(Odnomera)

---
+++ ActualHomework(FINAL)/C# Dir.init/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = { -1.5, 0.1, 12, 0, -2.2, 0.5, -1, 0.3 };
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] < 0)
+                {
+                    double ElMin = x[i];
+                }
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] > 0)
+                {
+                    double ElMax = x[i];
+                }
+            }
+        }
+    }
+}
