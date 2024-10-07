df268f6159570d894d58ff43f842b8b04d9b605f
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-07 23:48:24 +0300

    Update Homework1Lab4.cs

--- ActualHomework(FINAL)/C# Dir.init/Homework1Lab4.cs
+++ ActualHomework(FINAL)/C# Dir.init/Homework1Lab4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp //Определяем имя для программы
 {
@@ -8,36 +8,29 @@ namespace ConsoleApp //Определяем имя для программы
         {
             Console.WriteLine("Введите радиус, чтобы узнать стороны правильных 10 угольника, 50 угольника и 100 угольника И Н УГОЛЬНИКА");
             double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество углов для вычисления периметра N угольника");
             double n = Convert.ToDouble(Console.ReadLine());
 
             //Вычисление стороны правильного 10-ти угольника
-            double a1 = Radius(10, r);
+            double a1 = Perimeter(10, r);
             Console.WriteLine(" Вычисление стороны правильного 10 угольника = " + a1);
 
             //Вычисление стороны правильного 50-ти угольника
-            double a2 = Radius(50, r);
+            double a2 = Perimeter(50, r);
             Console.WriteLine(" Вычисление стороны правильного 50 угольника  = " + a2);
 
             //Вычисление стороны правильного 100 угольника
-            double a3 = Radius(100, r);
+            double a3 = Perimeter(100, r);
             Console.WriteLine(" Вычисление стороны правильного 100 угольника = " + a3);
 
-            Console.WriteLine("Вычисление стороны правильного n угольника, и его периметра");
-            double a4 = Radius(n, r);
-            Console.WriteLine("Вычисление стороны правильного n угольника = ", + a4);
-            double p1 = Perimeter(n, r);
-            Console.WriteLine("Вычисление стороны правильного n угольника, и его периметра", + p1);
+
+            double p = Perimeter(n, r);
+            Console.WriteLine("Perimetr = " +p);
         }
-        static double Radius(double n, double r) //Функция в  которой определяем сторону n угольника
+        static double Perimeter(double n, double r) //Функция в которой определяем периметр
         {
-            double a = 2 * r * Math.Sin(180 / n);
-            Console.WriteLine(" СТОРОНА ПРАВИЛЬНОГО Н УГОЛЬНИКА " + a);
-            return a;
-        }
-        static double Perimeter(double n, double a) //Функция в которой определяем периметр
-        {
-            double p = n * a;
-            Console.WriteLine(" Периметр = " + p);
+            double a = 2 * r * Math.Sin(180 / n); // нахождение правильной стороны
+            double p = n * a; // Вычисление периметра
             return p;
         }
     }
