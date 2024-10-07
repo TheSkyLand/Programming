6908463344bd4bbf6b74fceec16a338c97930b7e
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-08 00:23:22 +0300

    Update index.js

--- ActualHomework(FINAL)/JSdir.init/LAB 1 WORK 10/index.js
+++ ActualHomework(FINAL)/JSdir.init/LAB 1 WORK 10/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-let a;
+
 let x = Number(prompt("enter x = "));
 let y = Number(prompt("enter y = "));
-let e = Number(prompt("enter e = "));
-a = 0.8 * Math.pow(10, -5) * (x * Math.pow(Math.pow(e, -x*(y - 1.2) - y * x), 3));
+let a = 0.8 * Math.pow(10, -5) * Math.pow(x * Math.exp(-x * (y - 1.2)) - y * x, 3);
 console.log(a);
