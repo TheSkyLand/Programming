876ed8de04b8d762f220e923ff4a369a578eefa1
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-07 23:39:02 +0300

    Update HomeWork1Lab3.cs

--- ActualHomework(FINAL)/C# Dir.init/HomeWork1Lab3.cs
+++ ActualHomework(FINAL)/C# Dir.init/HomeWork1Lab3.cs
@@ -16,28 +16,41 @@ namespace Homwork1Lab3
                 if (Math.Pow(p, 2) - 1 <= 0)
                 {
                     y = Math.Pow(p, 2) - 1;
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
                 if (0 < p && p <= 1.5)
                 {
                     y = Math.Cos(p);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
                 if (p > 1.5)
                 {
                     y = Math.Sin(p - 1);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
-
             }
             Console.WriteLine("--------------------------");
             double x = -4;
             while (x <= 4)
             {
+
+
                 if (Math.Pow(x, 2) - 1 <= 0)
                 {
                     y = Math.Pow(x, 2) - 1;
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
 
                 x += 0.5;
@@ -45,7 +58,10 @@ namespace Homwork1Lab3
                 if (0 < x && x <= 1.5)
                 {
                     y = Math.Cos(x);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
 
                 x += 0.5;
@@ -53,8 +69,12 @@ namespace Homwork1Lab3
                 if (x > 1.5)
                 {
                     y = Math.Sin(x - 1);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
+
 
                 x += 0.5;
             }
@@ -67,7 +87,10 @@ namespace Homwork1Lab3
                 if (Math.Pow(x, 2) - 1 <= 0)
                 {
                     y = Math.Pow(x, 2) - 1;
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
 
                 x += 0.5;
@@ -75,7 +98,10 @@ namespace Homwork1Lab3
                 if (0 < x && x <= 1.5)
                 {
                     y = Math.Cos(x);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
 
                 x += 0.5;
@@ -83,7 +109,10 @@ namespace Homwork1Lab3
                 if (x > 1.5)
                 {
                     y = Math.Sin(x - 1);
-                    Console.WriteLine(y);
+                    if (y >= 0)
+                    {
+                        Console.WriteLine(y);
+                    }
                 }
 
                 x += 0.5;
