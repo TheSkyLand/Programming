de9838e6d1c44cdd530520115426b9d2b0939320
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-07 15:59:05 +0300

    Update HomeWork18Lab1.cs

--- ActualHomework(FINAL)/C# Dir.init/HomeWork18Lab1.cs
+++ ActualHomework(FINAL)/C# Dir.init/HomeWork18Lab1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection.Metadata.Ecma335;
 
 namespace HomeWork18
@@ -8,9 +8,9 @@ namespace HomeWork18
         static void Main(string[] args)
         {
             double f;
-            Console.WriteLine("y");
+            Console.WriteLine("введите y: ");
             double y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("x");
+            Console.WriteLine("введите x: ");
             double x = Convert.ToDouble(Console.ReadLine());
             f = Math.PI / 3 + (Math.Log(Math.Pow(x, 3) / 3 * y) - x) + x * Math.Sin(Math.Pow(y, 2));
             Console.WriteLine("Result: " + f);
