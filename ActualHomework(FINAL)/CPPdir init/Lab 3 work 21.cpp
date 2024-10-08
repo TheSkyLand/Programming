d8758f2a7feefc89340d1dc3e86de9e8f4fe9121
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-08 11:12:58 +0300

    Update Lab 3 work 21.cpp

--- ActualHomework(FINAL)/CPPdir init/Lab 3 work 21.cpp
+++ ActualHomework(FINAL)/CPPdir init/Lab 3 work 21.cpp
@@ -1,37 +1,36 @@
-﻿#include <iostream>
-#include <cmath>
-
+#include <iostream>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
     double y;
-    double x = 0;
     cout << "использование цикла for" << endl;
     for (double p = 6; p <= 8; p += 0.2)
     {
-        y = pow(log(p), 2 / p);
+        y = pow(log(p), 2) / p;
         cout << y << endl;
     }
 
     cout << " -------------------------- " << endl << endl;
     cout << " использование цикла while " << endl;
+    double x = 6;
 
     while (x <= 8)
     {
-        y = pow(log(x), 2 / x);
+        y = pow(log(x), 2) / x;
         x += 0.2;
         cout << y << endl;
     }
-    
+
     cout << " ---------------------------- " << endl << endl;
     cout << " использование цикла do while " << endl;
+    x = 6;
 
     do
     {
-        y = pow(log(x), 2 / x);
+        y = pow(log(x), 2) / x;
         x += 0.2;
-        cout << y << endl;
+        cout << (y) << endl;
     } while (x <= 8);
     system("pause");
 }
