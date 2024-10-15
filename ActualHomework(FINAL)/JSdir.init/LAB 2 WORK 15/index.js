30b393f90fcdb0ed08f7db550247c95d1d987d8a
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-15 09:18:47 +0300

    Update index.js

--- ActualHomework(FINAL)/JSdir.init/LAB 2 WORK 15/index.js
+++ ActualHomework(FINAL)/JSdir.init/LAB 2 WORK 15/index.js
@@ -1,11 +1,7 @@
 let s;
 let a = Number(prompt("Enter the value of a:"));
 let b = Number(prompt("Enter the value of b:"));
-
 let x = Number(prompt("Enter the value of x:"));
-let ElMax = Math.pow(x, 3); //Предположение
-let ElMin = Math.sin(x) / x; //Предположение
-let ElMax1 = Math.pow(a, x); //Предположение
 
 if (x < 2)
 {
@@ -14,6 +10,7 @@ if (x < 2)
 }
 if (x > 3)
 {
+    ElMax = Math.pow(x, 3)
     if (ElMax < Math.exp(x))
     {
         ElMax = Math.exp(x);
@@ -23,27 +20,31 @@ if (x > 3)
         ElMax = Math.pow(10, 3);
     }
     s = ElMax;
-    console.log("ElMin = " + ElMin);
     console.log("ElMax = " + ElMax);
     console.log("s = " + s);
 }
 else
 {
-    if (ElMin > ElMax1)
+    ElMin = Math.sin(x)/x
+    ElMax = Math.pow(a, x)
+    if (ElMin > ElMax)
     {
-        ElMin = ElMax1;
-        if (ElMax1 < Math.pow(x, 3))
+        ElMin = ElMax;
+        if (ElMax < Math.pow(x, 3))
             {
-                ElMax1 = Math.pow(x, 3);
+                ElMax = Math.pow(x, 3);
+                s = Elmax;
+                console.log("s = " + s);
+                console.log("ElMin = " + ElMin);
+                console.log("ElMax = " + ElMax);
             }
-    }        
+    }
     if (ElMin > x * Math.pow(Math.log(), 2) * x)
     {
         ElMin = x * Math.pow(Math.log(), 2) * x;
+        s = ElMin;
+        console.log("s = " + s);
+        console.log("ElMin = " + ElMin);
+        console.log("ElMax = " + ElMax);
     }
-    s = ElMin;
-    console.log("ElMin = " + ElMin);
-    console.log("ElMax1 = " + ElMax1);
-    console.log("s = " + s);
-    
 }
