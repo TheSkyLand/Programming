9d910750f3e2fc1aadb73e1832abc2e7c3956f07
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-15 09:32:12 +0300

    Update index.js

--- ActualHomework(FINAL)/JSdir.init/LAB 4 WORK 15/index.js
+++ ActualHomework(FINAL)/JSdir.init/LAB 4 WORK 15/index.js
@@ -1,12 +1,12 @@
-let a  = Number(prompt("Enter the value of a:"));
-let b = Number(prompt("Enter the value of b:"));
-let c = Number(prompt("Enter the value of c:"));
+let a = +prompt("Enter the value of a: ");
+let b = +prompt("Enter the value of b: ");
+let c = +prompt("Enter the value of c: ");
 let p = (a + b + c) / 2;
 let y = -c;
 let oppositeC = Math.tan(y) / 2;
 oppositeC = calculateArea(a, b, c);
 console.log(oppositeC);
-function calculateArea(a, b, c,) {
-    let area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    return area;
+function calculateArea(a, b, c,) 
+{
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 }
