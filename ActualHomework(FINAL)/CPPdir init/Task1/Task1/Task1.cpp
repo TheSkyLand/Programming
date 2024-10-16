b39d5093a8727d2aeac5d9a284b3adeaba671e2b
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-16 12:36:53 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/CPPdir init/Task1/Task1/Task1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double WrB, WrC, WrA, WrLa;
+	double Rb, Rc, Ra;
+	double La;
+
+	double Rb = 5;
+	double Rc = 2;
+	double Ra = 7;
+
+	La = sqrt(Rb * Rc) * (pow(Rb + (Rc), 2) - pow(Ra, 2)) / (Rb + Rc);
+	cout << "Task" << endl << endl;
+
+	cout << " При b = " << Rb << " при c =" << Rc << " при a = " << Ra << " La(b, c, a) = " << La << endl << endl;
+
+	//ЗАПРОС ДАННЫХ
+	cout << " Введите b: ";
+	cin >> WrB;
+
+	cout << " Введите c: ";
+	cin >> WrC;
+
+	cout << " Введите a: ";
+	cin >> WrA;
+
+	WrLa = operation(WrA, WrB, WrC);
+
+	cout << " При b = " << WrB << " при c =" << WrC << " при a = " << WrA << " La (b, c, a) = " << WrLa << endl << endl;
+}
+double operation(double Rb, double Rc, double Ra)
+{
+	return sqrt(Rb * Rc) * (pow(Rb + (Rc), 2) - pow(Ra, 2)) / (Rb + Rc);
+}
