510459fc53b707f857d7d929ad91862a12664797
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-20 01:22:08 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/CPPdir init/Task7/ConsoleApplication9/Task7_main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include "Task7_Func.h"
+using namespace std;
+
+
+int main()
+{
+	//Operation with data request
+	cout << "Enter b: ";
+	cin >> WrB;
+
+	cout << " Enter c: ";
+	cin >> WrC;
+
+	cout << " Enter a: ";
+	cin >> WrA;
+
+	operation();
+
+	cout << " so b = " << WrB << " so c = " << WrC << " so a = " << WrA << " La (b, c, a) = " << WrLa << endl << endl;
+}
