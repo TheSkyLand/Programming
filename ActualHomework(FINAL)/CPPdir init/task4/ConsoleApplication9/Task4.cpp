12edd7a1f1760ae33c220411212a5c1d961836f1
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-20 00:26:15 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/CPPdir init/task4/ConsoleApplication9/Task4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void operation();
+double WrC;
+double WrA;
+double WrB;
+double WrLa;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	//ÇÀÏÐÎÑ ÄÀÍÍÛÕ
+	cout << " Ââåäèòå b: ";
+	cin >> WrB;
+
+	cout << " Ââåäèòå c: ";
+	cin >> WrC;
+
+	cout << " Ââåäèòå a: ";
+	cin >> WrA;
+
+	operation();
+
+	cout << " Ïðè b = " << WrB << " ïðè c = " << WrC << " ïðè a = " << WrA << " La (b, c, a) = " << WrLa << endl << endl;
+}
+void operation()
+{
+	WrLa = sqrt(WrB * WrC) * (pow(WrB + (WrC), 2) - pow(WrA, 2)) / (WrB + WrC);
+}
