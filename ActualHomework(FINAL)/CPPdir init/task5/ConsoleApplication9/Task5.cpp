7f26e1d8543bbb6fc11f47da10c944461bc4ee51
George <77775699+TheSkyLand@users.noreply.github.com>
2024-10-20 00:34:17 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/CPPdir init/task5/ConsoleApplication9/Task5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+double operation(double, double, double);
+int main()
+{
+	double WrB, WrC, WrA, WrLa;
+	double La;
+
+	double Rb = 5;
+	double Rc = 2;
+	double Ra = 7;
+	//Standalone operation
+	La = operation(Rc, Ra, Rb);
+	cout << "Task 5" << endl << endl;
+
+	cout << " so b = " << Rb << " so c = " << Rc << " so a = " << Ra << " La(b, c, a) = " << La << endl << endl;
+
+	//Operation with data request
+	cout << "Enter b: ";
+	cin >> WrB;
+
+	cout << " Enter c: ";
+	cin >> WrC;
+
+	cout << " Enter a: ";
+	cin >> WrA;
+
+	WrLa = operation(WrA, WrB, WrC);
+
+	cout << " so b = " << WrB << " so c = " << WrC << " so a = " << WrA << " La (b, c, a) = " << WrLa << endl << endl;
+}
