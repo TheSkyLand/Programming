51cb2dbe3fcd3b505efd299a2a1c95b5b22912d0
George <77775699+TheSkyLand@users.noreply.github.com>
2024-12-25 16:28:19 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/WinFormsApp1/WinFormsApp1/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    internal class Class1
+    {
+        public int M
+        {
+            get { return m; }
+            set { m = value; }
+        }
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+        public double[,] A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        //количество
+        public int K
+        {
+            get { return k; }
+            set { k = n * m; }
+        }
+
+        private int k;
+
+
+        //переменная для суммы
+        public double sum;
+
+        //строчки
+        private int m;
+
+        //ряды
+        private int n;
+
+        //матрица
+        private double[,] a;
+
+        //переменная для количества
+        
+
+        //переменная для сохранения данных
+        protected double[] MaxEl;
+        public void MeanElement() 
+        {
+
+            MaxEl = new double[m];
+            for (int i = 0; i < m; i++)
+            {
+                MaxEl = new double[n];
+                for (int j = 0; j < n; j++)
+                { 
+                    sum += a[i, j];
+                }
+            }
+        }
+    }
+    public class ResSup : Class1
+    {
+        public void InputZ()
+        {
+            A = new double[M, N];
+            Random rand = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = rand.Next(-30, 30);
+                }
+            }
+        }
+        public void Output(ListBox LB)
+        {
+            LB.Items.Clear();
+            for(int i = 0; i < M; i++)
+            {
+                string s = "";
+                for(int j = 0; j < N; j++)
+                {
+                    if (A[i, j] >= 10)
+                        s += " " + A[i, j].ToString() + "  ";
+                    else if (A[i, j] > 0 && A[i, j] < 10)
+                        s += "   " + A[i, j].ToString() + "  ";
+                    else if (A[i, j] <= 0 && A[i, j] > -10)
+                        s += "  " + A[i, j].ToString() + "  ";
+                    else
+                        s += A[i, j].ToString() + "  ";
+                }
+                LB.Items.Add(s);
+            }
+        }
+    }
+}
---
+++ ActualHomework(FINAL)/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            listBox2 = new ListBox();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(89, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(167, 139);
+            textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(548, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(167, 139);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(89, 157);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 30);
+            button1.TabIndex = 2;
+            button1.Text = "жми";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(313, 222);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(167, 49);
+            listBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(313, 277);
+            button2.Name = "button2";
+            button2.Size = new Size(167, 30);
+            button2.TabIndex = 4;
+            button2.Text = "жми 2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(262, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 5;
+            label1.Text = "кол-во";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(listBox2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
+        private ListBox listBox2;
+        private Button button2;
+        private Label label1;
+    }
+}
