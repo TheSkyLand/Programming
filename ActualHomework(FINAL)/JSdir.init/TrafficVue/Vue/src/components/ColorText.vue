2572a37e25c4955bca356d844a115aa8e7a24072
George <77775699+TheSkyLand@users.noreply.github.com>
2025-01-14 21:41:26 +0300

    Add files via upload

---
+++ ActualHomework(FINAL)/JSdir.init/TrafficVue/Vue/src/components/ColorText.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :style="[`color: ${colorText}`]">
+        text = {{ mainText }}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    mainText: {
+        type: String,
+        required: true
+    },
+    colorText: {
+        type: Number,
+        required: true,
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ ActualHomework(FINAL)/JSdir.init/TrafficVue/Vue/src/views/ColorView.vue
@@ -0,0 +1,12 @@
+<template>
+<span>
+    <ColorText mainText="hi" colorText="red"/>
+</span>
+</template>
+
+<script setup>
+import ColorText from "../components/ColorText.vue";
+</script>
+
+<style scoped>
+</style>
