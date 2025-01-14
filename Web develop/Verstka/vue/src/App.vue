95051bad42909099e217f7d073148e393bc21ee0
George <77775699+TheSkyLand@users.noreply.github.com>
2025-01-14 10:20:08 +0300

    Add files via upload

--- Web develop/Verstka/vue/src/App.vue
+++ Web develop/Verstka/vue/src/App.vue
@@ -6,6 +6,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from './views/HomeView.vue';
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 
--- Web develop/Verstka/vue/src/router/index.js
+++ Web develop/Verstka/vue/src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WebVue from '../views/WebVue.vue'
-
+import Traffic from '../views/Traffic.vue'
+ 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,6 +15,23 @@ const router = createRouter({
       path: '/Web',
       name: 'Web',
       component: WebVue
+    },
+    {
+      path: '/traffic',
+      component: Traffic,      
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
